9c5da4c9a302d37f50b8c94c5cc2aeceb78a7b58
Rahul Sharma <rahul.hcst@outlook.com>
2016-12-23 18:09:43 +0530

    Emails table deleted

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,7 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'CreateEmailsTable' => $baseDir . '/database/migrations/2016_12_23_110224_create_emails_table.php',
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
     'DatabaseSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
