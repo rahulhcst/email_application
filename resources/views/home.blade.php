241eb65b910fdb7d0c9d893127de96e36804b27f
Rahul Sharma <rahul.hcst@outlook.com>
2016-12-24 07:56:20 +0530

    JS updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,14 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-3 col-md-2">
-                <a href="#" class="btn btn-danger btn-sm btn-block" role="button">COMPOSE</a>
+                <a href="#" class="btn btn-danger btn-sm btn-block" role="button" id="compose">COMPOSE</a>
                 <hr />
                 <ul class="nav nav-pills nav-stacked">
                     <li class="active"><a href="#"><span class="badge pull-right">42</span> Inbox </a>
                     </li>
-                    <li><a href="http://www.jquery2dotnet.com">Sent</a></li>
-                    <li><a href="http://www.jquery2dotnet.com">Drafts</a></li>
-                    <li><a href="http://www.jquery2dotnet.com"><span class="badge pull-right">3</span>Trash boxes</a></li>
+                    <li><a href="#">Sent</a></li>
+                    <li><a href="#">Drafts</a></li>
+                    <li><a href="#"><span class="badge pull-right">3</span>Trash boxes</a></li>
                 </ul>
             </div>
             <div class="col-sm-9 col-md-10">
@@ -71,6 +71,13 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div id="compose_window">>
+        <h4 class="header">New Message</h4>
+        <to,subject,body,>
+        <form>
+
+        </form>
     </div>
 
 @endsection
