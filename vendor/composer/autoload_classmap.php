1c046b89e6b0a752a17bb034ff4f54fe65aa3410
Rahul Sharma <rahul.hcst@outlook.com>
2016-12-27 06:46:23 +0530

    New migrations

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,11 +6,14 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'CreateDraftTable' => $baseDir . '/database/migrations/2016_12_27_011555_create_draft_table.php',
     'CreateEmailCategoryTable' => $baseDir . '/database/migrations/2016_12_23_130556_create_email_category_table.php',
     'CreateEmailRecordTable' => $baseDir . '/database/migrations/2016_12_23_180058_create_email_record_table.php',
     'CreateEmailRecordsTable' => $baseDir . '/database/migrations/2016_12_24_090828_create_email_records_table.php',
+    'CreateEmailThreadTable' => $baseDir . '/database/migrations/2016_12_27_010043_create_email_thread_table.php',
     'CreateEmailsTable' => $baseDir . '/database/migrations/2016_12_23_124010_create_emails_table.php',
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
+    'CreateReceiversTable' => $baseDir . '/database/migrations/2016_12_27_011511_create_receivers_table.php',
     'CreateUsersEmailMapper' => $baseDir . '/database/migrations/2016_12_23_133828_create_users_email_mapper.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
     'DatabaseSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
