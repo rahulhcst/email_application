61c97b51305764c43f7202f02bea1c1838aca70c
Rahul Sharma <rahul.hcst@outlook.com>
2016-12-27 19:15:56 +0530

    EmailThread Model Updated

--- app/EmailThread.php
+++ app/EmailThread.php
@@ -8,6 +8,8 @@ class EmailThread extends Model
 {
     protected $guarded = [];
 
+    public $table = 'email_thread';
+
     public function emailRecords()
     {
         return $this->hasMany(EmailRecord::class, 'thread_id', 'id');
